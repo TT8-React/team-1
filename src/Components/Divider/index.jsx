3a6300709cf46cbcaec0672f45508e73bd12aee8
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-02-16 17:28:45 +0200

    Add Divider

---
+++ src/Components/Divider/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+
+const Divider = styled.div`
+    width: 100%;
+    border-bottom: 1px solid #000000;
+    margin-bottom: 35px;
+    margin-top: 26px;
+
+`;
+function index() {
+  return (
+    <Divider/>
+  )
+}
+
+export default index
