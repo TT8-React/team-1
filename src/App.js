5646c025ecbf9f46f966921d39496335f5bd8348
Anas Badran <ansaccts@gmail.com>
2023-02-16 12:29:14 +0200

    added table component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Suspense } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom"; import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary'
-import Spinner from './Components/Spinners/Spinner'
-import { BaseLayout } from './Layout/BaseLayout'
-import ForgetPassword from './Pages/Auth/ForgetPassword';
-import Login from './Pages/Auth/Login'
-import Register from './Pages/Auth/Register';
-import Verification from './Pages/Auth/Verification';
-import Dashboard from './Pages/Home/Dashboard';
-import { RouteWrapper } from './routes/RouteWrapper'
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ErrorBoundary from "./Components/ErrorBoundary/ErrorBoundary";
+import Spinner from "./Components/Spinners/Spinner";
+import Table from "./Components/Table";
+import { BaseLayout } from "./Layout/BaseLayout";
+import ForgetPassword from "./Pages/Auth/ForgetPassword";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Auth/Register";
+import Verification from "./Pages/Auth/Verification";
+import Dashboard from "./Pages/Home/Dashboard";
+import { RouteWrapper } from "./routes/RouteWrapper";
+
+import "./App.css";
 
 function App() {
   return (
@@ -20,20 +20,41 @@ function App() {
         <ErrorBoundary>
           <Router>
             <Switch>
+              <Route path="/table" component={Table} />
               <Route path={"/login"} component={Login} />
               <Route path={"/register"} component={Register} />
               <Route path={"/forgetPassword"} component={ForgetPassword} />
               <Route path={"/verification"} component={Verification} />
-              <RouteWrapper path="/profile-details" exact component={Dashboard} layout={BaseLayout} />
-              <RouteWrapper path="/company-details" exact component={Dashboard} layout={BaseLayout} />
-              <RouteWrapper path="/report" exact component={Dashboard} layout={BaseLayout} />
-              <RouteWrapper path="/" exact component={Dashboard} layout={BaseLayout} />
+              <RouteWrapper
+                path="/profile-details"
+                exact
+                component={Dashboard}
+                layout={BaseLayout}
+              />
+              <RouteWrapper
+                path="/company-details"
+                exact
+                component={Dashboard}
+                layout={BaseLayout}
+              />
+              <RouteWrapper
+                path="/report"
+                exact
+                component={Dashboard}
+                layout={BaseLayout}
+              />
+              <RouteWrapper
+                path="/"
+                exact
+                component={Dashboard}
+                layout={BaseLayout}
+              />
             </Switch>
           </Router>
         </ErrorBoundary>
       </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Table/index.js
+++ src/Components/Table/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableField,
+  TableHead,
+  TableRow,
+  TableTitle,
+} from "./table.styles";
+import tableData from "./data.js";
+
+let nextId = 1;
 
 function Index() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          {tableData.header.map((item) => (
+            <TableTitle>{item}</TableTitle>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {Object.keys(tableData.rows).map((row) => (
+          <TableRow key={nextId++}>
+            {tableData.rows[row].map((rowField) => (
+              <TableField key={nextId++}>{rowField}</TableField>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
 
-export default Index
+export default Index;
